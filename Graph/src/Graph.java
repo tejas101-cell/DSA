06f5861016d269691589e65ab7760510bacd08c7
Tejas <panchaltejas253@gmail.com>
2025-12-24 12:08:56 +0530

    Starting with the graph

---
+++ Graph/src/Graph.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+// implementation from the gfg
+public class Graph {
+    // V -> number of vertices
+
+    public ArrayList<ArrayList<Integer>> createGraph(int V, int [][]edges){
+        ArrayList<ArrayList<Integer>> adjMat = new ArrayList<>();
+        int vert = V;
+        // initialize the matrix with 0
+        for (int i = 0; i<V; i++){
+                // nCopies gives the List
+                // number of elements will be V
+                // value of elements 0
+                ArrayList<Integer> row = new ArrayList<>(Collections.nCopies(V, 0));
+
+                adjMat.add(row);
+        }
+        // adding edges into the adjecency matrix
+        for (int [] arr : edges){
+            int u = arr[0];
+            int v = arr[1];
+
+            // getting the row index and then setting it's respective value as 1 (where it is applicable)
+            adjMat.get(u).set(v,1);
+            // same thing just if edge is undirected
+            adjMat.get(v).set(u,1);
+        }
+
+
+
+        return adjMat;
+    }
+    public void printAdjMat(ArrayList<ArrayList<Integer>> adjMat){
+        for (int i = 0; i<adjMat.size(); i++){
+            for(int j = 0; j<adjMat.size(); j++){
+                System.out.print(adjMat.get(i).get(j));
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Graph/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int v = 3;
+        int [][] edges = {{0,1},{0,2}};
+
+        Graph graph = new Graph();
+        ArrayList<ArrayList<Integer>> adjMat = graph.createGraph(v, edges);
+        graph.printAdjMat(adjMat);
+    }
+}
